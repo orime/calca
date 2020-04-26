b411e34b8c4ea6633be2d525f998d041ace75438
Orime小猪 <1126439586@qq.com>
2020-04-26 18:53:02 +0800

    '提交打包文件'

---
+++ build/precache-manifest.867f72638c1d1cb6faead0f7500c4ba5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00dea3e56938f078015a7804e293da5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3c11430acbcd8dceed19",
+    "url": "./static/css/2.da835491.chunk.css"
+  },
+  {
+    "revision": "e1950521b98b0de2ef0e",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3c11430acbcd8dceed19",
+    "url": "./static/js/2.bbb0a97c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.bbb0a97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1950521b98b0de2ef0e",
+    "url": "./static/js/main.1b62bba4.chunk.js"
+  },
+  {
+    "revision": "7a54205dee0c1c32f9f9",
+    "url": "./static/js/runtime-main.e2bbe5ca.js"
+  }
+]);
