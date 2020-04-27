9e28560f18b02e894cf88c0e51c26cd3d460965e
Orime小猪 <1126439586@qq.com>
2020-04-27 13:39:02 +0800

    1、实现布局自由切换 2、暴露eject配置 3、防抖延迟降低至300ms

---
+++ build/precache-manifest.829de07828d33150dff3ce6899e9eed5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f499477dd0f2989f58c68ea17ed6eb2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8f97e3bde9cf980ccb05",
+    "url": "./static/css/2.8c44a2b2.chunk.css"
+  },
+  {
+    "revision": "86e1a5034c8ad6a19972",
+    "url": "./static/css/main.20948ded.chunk.css"
+  },
+  {
+    "revision": "8f97e3bde9cf980ccb05",
+    "url": "./static/js/2.bdfe0a55.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.bdfe0a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86e1a5034c8ad6a19972",
+    "url": "./static/js/main.7ecee9be.chunk.js"
+  },
+  {
+    "revision": "7a54205dee0c1c32f9f9",
+    "url": "./static/js/runtime-main.e2bbe5ca.js"
+  }
+]);
